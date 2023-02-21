bc012eb43776737b3b528bee089dc7edfde5ee00
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2023-02-20 21:49:55 -0600

    integration with grafana and influxdb.

--- checkChallenge.js
+++ checkChallenge.js
@@ -1,27 +1,31 @@
 import { check } from "k6";
 import http from "k6/http";
 
-const TIMINGS_DURATION = 2000;
+const TIMINGS_DURATION = 500;
 
 export const options = {
     vus: 10,
-    duration: '20s',
+    duration: '10s',
     thresholds: {
-        http_req_failed: ["rate<0.1"],
+        // 1st threshold: evaluate at final
+        http_req_failed: ["rate<0.35"],
+        // 2nd threshold: evaluate at 10 seconds if fail
         http_req_duration: [{
             threshold: `p(95)<${TIMINGS_DURATION}`,
             abortOnFail: true,
             delayAbortEval: "10s",// after 10s started, if there's a fail, abort.
-        }],// 95% request < 200ms
-    }
+        }],
+    },
 };
 
 export default function () {
-    const response = http.get(`https://api.escuelajs.co/api/v1/products/6`);
+    const PRODUCT_NOT_FOUND = 0;
+    const PRODUCT_RANDOM = parseInt(Math.random()*47) + 295 + PRODUCT_NOT_FOUND;//  [295-342] = 47 products
+    const RESPONSE = http.get(`https://api.escuelajs.co/api/v1/products/${PRODUCT_RANDOM}`);
 
-    check(response, {
+    check(RESPONSE, {
         "statusCode is 200": (r) => r.status === 200,
-        "transaction time ": (r) => r.timings.duration < TIMINGS_DURATION,
+        "transaction is below 500mx ": (r) => r.timings.duration < TIMINGS_DURATION,
     });
 };
 
--- stress-test.js
+++ stress-test.js
@@ -3,10 +3,10 @@ import http from "k6/http";
 
 export const options = {
     stages: [
-        { duration: '2m', target: 100 },
-        { duration: '3m', target: 150 },
-        { duration: '2m', target: 200 },
-        { duration: '2m', target: 200 }
+        { duration: '20s', target: 100 },
+        { duration: '10s', target: 150 },
+        { duration: '10s', target: 300 },
+        { duration: '10s', target: 400 }
     ]
 };
 options.stages.length
@@ -14,6 +14,6 @@ options.stages.length
 export default function () {
     let response = http.get('https://api.escuelajs.co/api/v1/categories');
 
-    sleep(1);// sleep 1 second
+    //sleep(1);// sleep 1 second
 };
 
